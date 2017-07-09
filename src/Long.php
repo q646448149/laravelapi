c996e26ef4619fcbba9b29466618ae3ab31dee50
master <master@qq.com>
2017-07-09 22:27:12 +0800

    composer_ok

---
+++ src/Long.php
@@ -0,0 +1,10 @@
+<?php
+namespace Long;
+class Long
+{
+
+    public static function test()
+    {
+        return 'class long test function';
+    }
+}
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+echo \Long\Long::test();
+
+
